8108b9570ee1a32f1a00254d258d614e518282dc
arteeeemt <kesha514@ukr.net>
2023-12-27 18:02:16 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Provider } from 'react-redux'; // Импортируйте Provider
+import {store} from 'red/store.js'; // Импортируйте ваш созданный Redux-стор
+
+import { Form } from "./Form/Form";
+import { ContactsList } from "./ContactsList/ContactsList";
+import { Filter } from "./Filter/Filter";
+import { Container } from "./App.styled";
+
+export const App = () => (
+  <Provider store={store}> {/* Оберните приложение в Provider */}
+    <Container>
+      <h1>Phonebook</h1>
+      <Form />
+      <h2>Contacts</h2>
+      <Filter />
+      <ContactsList />
+    </Container>
+  </Provider>
+);
